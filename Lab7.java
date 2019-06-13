9f09bea5e2b595966e0bb80cecfd013103e2a9ea
Laserarjun876 <laserarjun876@gmail.com>
2019-06-13 19:46:15 +0545

    two new dfa

--- Lab7.java
+++ Lab7.java
@@ -1,9 +1,9 @@
-//string such that every string accepted must have length |w|=2(mod 3)
+//dfa that accepts string such that accepted string must have length of a  |w|a=3(mod 4)
 package toc_lab_dfa;
 
 import java.util.Scanner;
 
-public class Lab6 {
+public class Lab7 {
 	public static void main(String[] args) {
 		int q=0,i=0;
 		System.out.println("Enter any string :");
@@ -18,7 +18,7 @@ public class Lab6 {
 			}
 			else if(q==0 && ch=='b')
 			{
-			q=1;
+			q=0;
 			}
 			else if(q==1 && ch=='a')
 			{
@@ -26,28 +26,34 @@ public class Lab6 {
 			}
 			else if(q==1 && ch=='b')
 			{
-			q=2;		
+			q=1;		
 			}
 			else if(q==2 && ch=='a')
 			{
-			q=0;
+			q=3;
 			}
 			else if(q==2 && ch=='b')
 			{
-			q=0;
+			q=2;
 			}
+			else if(q==3 && ch=='a')
+			{
+				q=0;
+				}
+				else if(q==3 && ch=='b')
+				{
+				q=3;
+				}
 				else
 				{
 				q=4;
 				break;
 				}
 		}
-		if(q==2)		
+		if(q==3)		
 			System.out.println("Accepted.");
 		else
 			System.out.println("Rejected.");
-	}
 
-
-
+		}
 }
--- Lab8.java
+++ Lab8.java
@@ -1,9 +1,8 @@
-//string such that every string accepted must have length |w|=2(mod 3)
 package toc_lab_dfa;
 
 import java.util.Scanner;
 
-public class Lab6 {
+public class Lab8 {
 	public static void main(String[] args) {
 		int q=0,i=0;
 		System.out.println("Enter any string :");
@@ -18,11 +17,11 @@ public class Lab6 {
 			}
 			else if(q==0 && ch=='b')
 			{
-			q=1;
+			q=0;
 			}
 			else if(q==1 && ch=='a')
 			{
-			q=2;
+			q=1;
 			}
 			else if(q==1 && ch=='b')
 			{
@@ -30,24 +29,30 @@ public class Lab6 {
 			}
 			else if(q==2 && ch=='a')
 			{
-			q=0;
+			q=3;
 			}
 			else if(q==2 && ch=='b')
 			{
-			q=0;
+			q=2;
 			}
+			else if(q==3 && ch=='a')
+			{
+				q=3;
+				}
+				else if(q==3 && ch=='b')
+				{
+				q=0;
+				}
 				else
 				{
 				q=4;
 				break;
 				}
 		}
-		if(q==2)		
+		if(q==2 || q==3)		
 			System.out.println("Accepted.");
 		else
 			System.out.println("Rejected.");
-	}
 
-
-
+		}
 }
